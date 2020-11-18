61fc90a1bf7ac400b92a1129d7a018336b7aac0a
Sarvis <iamsarvis@gmail.com>
2020-11-18 16:44:41 +0330

    settings.php was created and the website information was transferred from function.php to this file

---
+++ settings.php
@@ -0,0 +1,9 @@
+<?php
+    // title of your website
+	$site_title = 'WP REST API';
+    // url of your website
+    $web = 'http://localhost/wp-rest-api-in-php';
+    // url of wordpress website
+    $url = 'http://localhost/sarvis';
+
+?>
