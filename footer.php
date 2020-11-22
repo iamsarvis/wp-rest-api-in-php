096e0ae405793f91f82e61699323464ddfc527cc
Sarvis <iamsarvis@gmail.com>
2020-11-22 17:11:28 +0330

    added footer section

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
+<footer class="footer container">
+    <div class="row">
+        <div class="col-4">hello</div>
+        <div class="col-4">hello2</div>
+        <div class="col-4">hello3</div>
+    </div>
+</footer>
 <script src="../assets/jquery.min.js"></script>
 <script src="../assets/bootstrap.min.js"></script>
 </body>
