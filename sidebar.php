99e63156e11ea788a65771693f7a41df2907639d
Sobhan Askari <sobhan0a0@gmail.com>
2020-11-24 00:02:08 +0330

    Update sidebar.php
    The links in the categories section have been edited

--- sidebar.php
+++ sidebar.php
@@ -9,7 +9,7 @@
                 if($category->parent == 0){
                     $category_id = $category->id;
                     echo '
-                    <a href="'.$web.'/category.php?id='.$category_id.'"><li class="list-group-item d-flex justify-content-between align-items-center">
+                    <a href="'.$web.'/category/'.$category_id.'"><li class="list-group-item d-flex justify-content-between align-items-center">
                         '.$category->name.'
                         <span class="badge badge-primary badge-pill">'.$category->count.'</span>
                     </li></a>';
@@ -19,7 +19,7 @@
                         if($category_child->parent != 0 && $category_parent == $category_child->parent){
                             $category_child_id = $category_child->id;
                             echo '
-                            <a href="'.$web.'/category.php?id='.$category_child_id.'"><li class="list-group-item list-group-item-secondary d-flex justify-content-between align-items-center">
+                            <a href="'.$web.'/category/'.$category_child_id.'"><li class="list-group-item list-group-item-secondary d-flex justify-content-between align-items-center">
                                 - '.$category_child->name.'
                                 <span class="badge badge-primary badge-pill">'.$category_child->count.'</span>
                             </li></a>';
