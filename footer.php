04b280881d701124de774d7ae6b804c1a7e57783
Sobhan Askari <sobhan0a0@gmail.com>
2020-11-23 23:17:42 +0330

    Update footer.php
    edited footer Copy-right

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="footer bg-light">
     <div class="footer-copyright text-center py-3">© 2020 Copyright:
-        <a href="http://iamsarvis.ir"> Sarvis</a>
+        <a href="<?php $web ?>"> $site_title</a>
     </div>
 </footer>
 <script src="../assets/jquery.min.js"></script>
